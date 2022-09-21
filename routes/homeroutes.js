6d24665a61058bfc189c1ea62c67ef49a0cb35b2
Benjamin Bushman <benbushman98@gmail.com>
2022-09-20 20:26:35 -0600

    added login and signup

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -4,6 +4,14 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.render('homepage');
   });
+
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
   
 
 module.exports = router;
