7c6a44075f49d6cfed8947dbe88ba169b2c878f7
Randie <srandall1213@gmail.com>
2022-09-21 11:29:55 -0600

    added signup event in JS

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const login = async(event) => {
     event.preventDefault();
     
-    const email = $('#');
-    const password = $('#');
+    const email = $('#emailVal');
+    const password = $('#passwordVal');
     
     if (email && password) {
       const response = await fetch('/api/user/login', {
@@ -20,4 +20,29 @@ const login = async(event) => {
         alert('Username or Password is Incorrect. If you do not have an account, please sign up.');
       }
     }
-  };
+};
+
+const signUp = async (event) => {
+  event.preventDefault();
+
+  const name = $('#signupName').value.trim();
+  const email = $('#signupEmail').value.trim();
+  const password = $('#signupPassword').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+  
+$("#loginBtn").click(login);
+$("#signupBtn").click(signUp);
