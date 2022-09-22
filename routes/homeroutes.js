7cbfffd56479b017b2c026c99e880d9f4177e55f
DinaLo <dinalo.brady@gmail.com>
2022-09-21 21:05:34 -0600

    working on get route for carousel

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {Survey, Question} = require('../models');
 
 // GET for Homepage
 router.get('/', (req, res) => {
@@ -13,5 +14,33 @@ router.get('/', (req, res) => {
     res.render('signup', {layout: 'main2'}) 
   });
   
+  // We wan tto get the survey id to be pulled up automatically
+  router.get('/survey/:id', async (req, res) => {
+    try{
+      const allSurveysData = await Survey.findByPk(req.params.id,{
+        include: [
+          {
+            model: Question,
+            attributes: ['question_body', 'response'],
+          },
+        ],
+       
+      });
+      if (!allSurveysData) {
+        console.log(error, 'Not found')
+      }
+      const survey =  allSurveysData.get({plain:true});
+      res.render('singlesurvey', {
+        ...survey
+        }
+      )
+    // res.status(200).json(allSurveysData);
+    // allSurveysData.map((survey) =>
+   
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  })
 
 module.exports = router;
