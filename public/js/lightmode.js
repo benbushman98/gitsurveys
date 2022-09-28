e2512e07744a8c940b6ed456a42838ff711edc23
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-09-28 17:55:13 +0900

    added lightmode

--- public/js/lightmode.js
+++ public/js/lightmode.js
@@ -1,8 +1,20 @@
+let mode = "dark";
+
 const toggleLightMode = async () => {
    console.log("check")
-   const link = document.getElementById("css-light-switch");
-   link.setAttribute("href", "/css/style-light.css");
-  };
+   let link = document.getElementById("css-light-switch");
+   let nav = document.getElementById("nav");
+   
+    if (mode === "dark") {
+       mode = "light";
+       link.setAttribute("href", "/css/style-light.css");
+       nav.setAttribute("class", "navbar fixed-top navbar-expand-lg bg-yellow");
+    }
+    else {
+        mode = "dark";
+        link.setAttribute("href", "/css/style.css");
+    }
+};
 
 const toggleBtn = document.getElementById('toggle');
 toggleBtn.addEventListener('click', toggleLightMode);
